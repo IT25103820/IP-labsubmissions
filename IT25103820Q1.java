bb76ea66996dc32c8020f2456939fe37a9abfdc4
IT25103820 <IT25103820@my.sliit.lk>
2026-02-08 21:39:34 +0530

    Add IT25103820_Lab_Q1

---
+++ IT25103820Q1.java
@@ -0,0 +1,16 @@
+public class IT25103820Q1{
+	public static void main(String[] args){
+		
+		int miles,yards=0;
+		double kilometers=0.0;
+		
+		miles=26;
+		yards=385;
+		
+		kilometer=(miles*1.609)+ ((yards/1760.0)+1.609);
+		
+		
+		
+		
+	}
+}
