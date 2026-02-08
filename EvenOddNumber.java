0b7a1ad572fbee546299ec67b486199db04bc9f1
IT25103820 <IT25103820@my.sliit.lk>
2026-02-08 21:38:42 +0530

    Add IT25103820_ Lab_Q2

---
+++ EvenOddNumber.java
@@ -0,0 +1,36 @@
+
+public class EvenOddNumber{
+	public static boolean findEvenOrOdd(int i){
+		return(i%2 ==0)
+		
+		
+	}
+	{
+	public class Demo {
+		
+		public static void main(String[] args) {
+			
+			EvenOddNumber checker = new EvenOddNumber();
+			
+			int[] testNumbers = {10, 7, 26, 385}; 
+			
+			
+			for (int num : testNumbers) {
+				
+			boolean result = checker.findEvenOrOdd(num); 
+			if (result) {
+				System.out.println(num + " is Even"); 
+				}
+				else
+					{ 
+				System.out.println(num + " is Odd"); 
+				} 
+		} 
+	} 
+				}
+	
+	
+	
+}
+
+}
